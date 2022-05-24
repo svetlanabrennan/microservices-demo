08c767fc63e4089c0312892da2c74a04d53b2fe9
Svetlana Brennan <svetlana.svn@gmail.com>
2022-05-24 16:34:47 -0500

    test deploy workflow

--- .github/workflows/install-dependencies.sh
+++ .github/workflows/install-dependencies.sh
@@ -34,7 +34,7 @@ sudo apt-get install -y dotnet-sdk-6.0
 echo "✅ dotnet installed"
 
 # install kubectl
-sudo apt-get install kubectl
+brew install kubectl
 echo "✅ kubectl installed"
 
 # install go
