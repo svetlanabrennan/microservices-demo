5929368b732a371d798f91c2e059e7ad183c1d3c
Svetlana Brennan <svetlana.svn@gmail.com>
2022-05-24 16:37:14 -0500

    test deploy workflow

--- .github/workflows/install-dependencies.sh
+++ .github/workflows/install-dependencies.sh
@@ -62,13 +62,13 @@ sudo mv skaffold /usr/local/bin
 echo "✅ skaffold installed"
 
 # install docker
-sudo apt install -yqq apt-transport-https ca-certificates curl gnupg2 software-properties-common && \
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && \
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && \
-sudo apt-get update && \
-sudo apt-get install -yqq docker-ce && \
-sudo usermod -aG docker ${USER}
-echo "✅ docker installed, rebooting..."
+# sudo apt install -yqq apt-transport-https ca-certificates curl gnupg2 software-properties-common && \
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && \
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && \
+# sudo apt-get update && \
+# sudo apt-get install -yqq docker-ce && \
+# sudo usermod -aG docker ${USER}
+# echo "✅ docker installed, rebooting..."
 
 # reboot for docker setup
-sudo reboot
+# sudo reboot
